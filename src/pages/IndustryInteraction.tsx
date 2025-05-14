8cf2d199ab45812c995ba78e7441dfa7c4be09fe
VenkatBabu95 <idukudavenkatapathi@gmail.com>
2025-05-14 15:33:54 +0530

    Updated pages: Achievements, Research, Faculty, and Index. Added Research page and enhanced UI/UX.

--- src/pages/IndustryInteraction.tsx
+++ src/pages/IndustryInteraction.tsx
@@ -171,7 +171,7 @@ const IndustryInteraction = () => {
       organization: "StartupIndia",
       interactionType: "Internship",
       date: "January - March, 2025",
-      certificate: "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+      certificate: "/nvin.jpeg"
     },
     {
       id: 7,
