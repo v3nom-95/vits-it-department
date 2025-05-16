faa8e3e7d6ee3c743e0287abb37b4f1614999e38
VenkatBabu95 <idukudavenkatapathi@gmail.com>
2025-05-16 12:53:42 +0530

    Update Footer: fix department name formatting and details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
-        <p>©  All rights reserved to Department IT, VITS, Deshmukh</p>
+        <p>©  All rights reserved to Department of IT, VITS, Deshmukh</p>
       </div>
     </footer>
   );
